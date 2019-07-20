c6cc929ad515393b6266a0911ebe2e09865faa1b
eleye <elyeleye@gmail.com>
2019-07-20 17:41:08 +0200

    second add

---
+++ vars/templates.groovy
@@ -0,0 +1,16 @@
+def standardTemplate(overrideApp) {
+	return [
+		DeployEnv: [
+			val: [ name : 'recette validation', deploy: true ],
+			rec: [ name : 'recette projet', deploy: false ],
+			preprod: [ name : 'recette preprod', deploy: false  ]
+		]
+
+
+	] << overrideApp
+}
+
+
+
+
+
