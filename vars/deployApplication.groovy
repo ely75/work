acf2e0656c0410f304ac4e3618a20ff446fb645f
eleye <elyeleye@gmail.com>
2019-07-20 20:42:29 +0200

    second add

--- vars/deployApplication.groovy
+++ vars/deployApplication.groovy
@@ -44,7 +44,7 @@ def deploy = { environment, environmentInformations ->
 							script {
 								def deployBranches = [:]
 								Appconfig.DeployEnv.each {
-								def x = "deploy_${it.key}"
+								def x = "deploy_it.key"
 								echo " env $x "
 									if( ${params.x } ) {
 										deployBranches[it.value.name] = { deploy it.key, it.value.deploy }
